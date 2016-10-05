dd2e17790b7a9d2c97d4cf347eb9e7b1d188c2b2
Islam Elsayed <islam.elsayed@badrit.com>
2016-10-05 15:33:17 +0200

    syntax update

--- lib/generators/review/templates/review.rb
+++ lib/generators/review/templates/review.rb
@@ -1,15 +1,11 @@
 class Review < ActiveRecord::Base
-
   include ActsAsReviewable::Review
 
-  belongs_to :reviewable, :polymorphic => true
-  belongs_to :reviewer, :polymorphic => true
-
-  default_scope :order => 'created_at ASC'
+  belongs_to :reviewable, polymorphic: true
+  belongs_to :reviewer, polymorphic: true
 
   # NOTE: install the acts_as_votable plugin if you
   # want votes on reviews.
-  #acts_as_voteable
+  # acts_as_voteable
 
 end
-
